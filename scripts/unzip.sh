a22760c8ab015014c8afc2af56110628d36cd0da
Bandita <bandita.adhikari@gmail.com>
2019-09-09 20:37:53 -0400

    Update unzip.sh

--- scripts/unzip.sh
+++ scripts/unzip.sh
@@ -1,1 +1,13 @@
 
+#Unzip files
+
+#!/bin/bash
+#PBS -q batch
+#PBS -l nodes=1:ppn=8
+#PBS -l walltime=5:00:00
+#PBS -N control
+#PBS -m ea
+#PBS -j oe
+#PBS -M c-adhikb@jax.org
+cd /your_directory_where_zipped/alignment_files_are/
+gunzip *.gz
